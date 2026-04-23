e51942f662635fa7c8db335654aa3e473fb6e539
LucaRotter <lgrotter@studenti.uninsubria.it>
2026-04-23 14:48:31 +0200

    implementazione di un chip

---
+++ Halo2_test/src/mathchip.rs
@@ -0,0 +1,124 @@
+use halo2_proofs::{circuit::*, plonk::*, poly::Rotation};
+use std::marker::PhantomData;
+use halo2_proofs::arithmetic::Field;
+use ff::PrimeField;
+
+
+pub struct MathChip{
+    pub a: Column<Advice>,
+    pub b: Column<Advice>,
+    pub out: Column<Advice>,//risultato intermedio
+    pub res: Column<Instance>,//risultato finale
+    pub s_mul: Selector,
+    pub s_mux: Selector,
+    pub s_add: Selector,
+    pub bit : Column<Advice>,//colonna per il bit di controllo del mux
+}
+
+pub struct MathChip<F: PrimeField> {
+    config: MathConfig,
+    _marker: PhantomData<F>,
+}
+
+impl<F: PrimeField> MathChip<F> {
+    pub fn construct(config: MathConfig) -> Self {
+        Self { config, _marker: PhantomData }
+    }
+
+    pub fn configure(meta: &mut ConstraintSystem<F>) -> MathConfig {
+        let a = meta.advice_column();
+        let b = meta.advice_column();
+        let out = meta.advice_column();
+        let bit = meta.advice_column();
+        let res = meta.instance_column();
+        
+        let s_mul = meta.selector();
+        let s_add = meta.selector();
+        let s_mux = meta.selector();
+
+        // GATE MOLTIPLICAZIONE
+        meta.create_gate("mul", |meta| {
+            let s = meta.query_selector(s_mul);
+            let lhs = meta.query_advice(a, Rotation::cur());
+            let rhs = meta.query_advice(b, Rotation::cur());
+            let out = meta.query_advice(out, Rotation::cur());
+            vec![s * (lhs * rhs - out)]
+        });
+
+        // GATE ADDIZIONE
+        meta.create_gate("add", |meta| {
+            let s = meta.query_selector(s_add);
+            let lhs = meta.query_advice(a, Rotation::cur());
+            let rhs = meta.query_advice(b, Rotation::cur());
+            let out = meta.query_advice(out, Rotation::cur());
+            vec![s * (lhs + rhs - out)]
+        });
+
+        // GATE MUX (If-Else)
+        meta.create_gate("mux", |meta| {
+            let s = meta.query_selector(s_mux);
+            let val_a = meta.query_advice(a, Rotation::cur());
+            let val_b = meta.query_advice(b, Rotation::cur());
+            let b_ctrl = meta.query_advice(bit, Rotation::cur());
+            let out = meta.query_advice(out, Rotation::cur());
+            let one = Expression::Constant(F::from(1u64));
+
+            vec![
+                s.clone() * b_ctrl.clone() * (one.clone() - b_ctrl.clone()), // bool check
+                s * ((one - b_ctrl.clone()) * val_a + (b_ctrl * val_b) - out) // mux logic
+            ]
+        });
+
+        MathConfig { a, b, out, res, s_mul, s_add, s_mux, bit }
+    }
+
+    pub fn do_mul(&self, mut layouter: impl Layouter<F>, a: Value<F>, b: Value<F>) -> Result<(), Error> {
+        layouter.assign_region(|| "mul operation", |mut region| {// || è una closure che prende in input un regione restituisce un Result
+            self.config.s_mul.enable(&mut region, 0)?;
+            region.assign_advice(|| "a", self.config.a, 0, || a)?;
+            region.assign_advice(|| "b", self.config.b, 0, || b)?;
+            // Calcoliamo il risultato per l'advice 'out'
+            let res = a.zip(b).map(|(a, b)| a * b);
+            region.assign_advice(|| "out", self.config.out, 0, || res)?;
+            Ok(())
+        })
+    }
+
+    pub fn do_add(&self, mut layouter: impl Layouter<F>, a: Value<F>, b: Value<F>) -> Result<(), Error> {
+        layouter.assign_region(|| "add operation", |mut region| {
+            self.config.s_add.enable(&mut region, 0)?;
+            region.assign_advice(|| "a", self.config.a, 0, || a)?;
+            region.assign_advice(|| "b", self.config.b, 0, || b)?;
+            // Calcoliamo il risultato per l'advice 'out'
+            let res = a.zip(b).map(|(a, b)| a + b);
+            let cell= region.assign_advice(|| "out", self.config.out, 0, || res)?;
+            Ok(cell)
+        })
+    }
+
+    pub fn do_mux(&self, mut layouter: impl Layouter<F>, a: Value<F>, b: Value<F>, bit: Value<F>) -> Result<(), Error> {
+        layouter.assign_region(|| "mux operation", |mut region| {
+            self.config.s_mux.enable(&mut region, 0)?;
+            region.assign_advice(|| "a", self.config.a, 0, || a)?;
+            region.assign_advice(|| "b", self.config.b, 0, || b)?;
+            region.assign_advice(|| "bit", self.config.bit, 0, || bit)?;
+            // Calcoliamo il risultato per l'advice 'out'
+            let res = a.zip(b).zip(bit).map(|((a, b), bit)| if bit == F::from(1u64) { b } else { a });
+            region.assign_advice(|| "out", self.config.out, 0, || res)?;
+            Ok(())
+        })
+    }
+
+    pub fn synthesize(&self,config: Self::Config, mut layouter: impl Layouter<F>) -> Result<(), Error> {
+        let chip= MathChip::construct(config);
+        //addizione
+        let somma = self.a.zip(self.b).map(|(a, b)| a + b);
+        let add_cell = chip.do_add(layouter.namespace(|| "addizione"), self.a, self.b)?;
+
+        chip.do_mul(layouter.namespace(|| "moltiplicazione"), add_cell.value(), self.c)?;
+
+        chip.do_mux(layouter.namespace(|| "mux"), somma, self.c, self.bit)?;
+        Ok(())
+    }
+
+}
